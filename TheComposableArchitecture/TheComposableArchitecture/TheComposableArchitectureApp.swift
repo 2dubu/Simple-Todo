0b22311e6d282e66461e79165a10d86d73d938a4
2dubu <2dubu.dev@gmail.com>
2023-02-04 23:59:04 +0900

    :wrench: Add xcode project file

---
+++ TheComposableArchitecture/TheComposableArchitecture/TheComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheComposableArchitectureApp.swift
+//  TheComposableArchitecture
+//
+//  Created by 이건우 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct TheComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
