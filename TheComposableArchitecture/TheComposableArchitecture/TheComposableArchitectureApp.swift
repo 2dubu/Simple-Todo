ccbda14d85b75a55901383dd20cbe0f4c8b28eca
2dubu <2dubu.dev@gmail.com>
2023-04-03 23:59:40 +0900

    :recycle: :: Code style

--- TheComposableArchitecture/TheComposableArchitecture/TheComposableArchitectureApp.swift
+++ TheComposableArchitecture/TheComposableArchitecture/TheComposableArchitectureApp.swift
@@ -4,15 +4,17 @@ import ComposableArchitecture
 @main
 struct TheComposableArchitectureApp: App {
     var body: some Scene {
+        let todoStore = Store(
+            initialState: AppState(todos: [
+                Todo(title: "Buy pizza"),
+                Todo(title: "Take out the trash")
+            ]),
+            reducer: appReducer,
+            environment: ()
+        )
+        
         WindowGroup {
-            TodoListView(store: Store(
-                initialState: AppState(todos: [
-                    Todo(title: "Buy pizza"),
-                    Todo(title: "Take out the trash")
-                ]),
-                reducer: appReducer,
-                environment: ()
-            ))
+            TodoListView(store: todoStore)
         }
     }
 }
